b33068446b6d19fac7ed7074efe871e566100aed
swaroop reddy <reddy.swaroop2016@gmail.com>
2021-01-28 12:10:46 -0600

    initial

---
+++ init-locations.js
@@ -0,0 +1,11 @@
+export default [{
+    Name: 'Franken Hall',
+    Latitude: 40.35435646901969,
+    Longitude:  -94.8884037021279 
+},
+{
+    Name: 'Hughes Fieldhouse',
+    Latitude: 40.3525577346379, 
+    Longitude: -94.88848953281591
+}
+];
